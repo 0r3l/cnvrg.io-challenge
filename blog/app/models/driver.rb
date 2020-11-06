1633223bf95433edbc39dc8014a0555ef046d919
Eliyahu, Orel <oe530k@intl.att.com>
2020-11-06 23:00:18 +0200

    add missing html elements

--- blog/app/models/driver.rb
+++ blog/app/models/driver.rb
@@ -1,6 +1,6 @@
 class Driver < ApplicationRecord
   validates :name, presence: true
-  validates :email, presence: true
+  validates :email, presence: true, format: { with: URI::MailTo::EMAIL_REGEXP }
   has_many :drivers_cars
   has_many :cars, through: :drivers_cars, dependent: :destroy
   has_attached_file :image, styles: { medium: "300x300>", thumb: "100x100>" }, default_url: "/images/:style/missing.png"
